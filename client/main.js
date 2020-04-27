a5a99350d6d35a5ea7a12b1ca9fc3c6f2310b9a3
victorgorban <victorgorban2@ya.ru>
2020-04-28 00:32:48 +0300

    hello world

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import App from './App.vue';
+import VueMeteorTracker from 'vue-meteor-tracker';   // here!
+Vue.use(VueMeteorTracker);
+import './main.html';
+
+Meteor.startup(() => {
+  new Vue({
+    el: '#app',
+    ...App,
+  });
+});
